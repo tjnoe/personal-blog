1a3dd19924ac2532b4401c1b695464a3d8b9daf4
Tommy Noe <tnoe@levdigital.com>
2020-01-05 20:16:33 -0500

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,15 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Tommy Noe's Blog`,
+    description: `Tommy's developer blog, sharing tidbits of knowledge he's learning along his journey`,
+    author: `@tommynoe`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`
+      },
+    },
+  ],
 }
