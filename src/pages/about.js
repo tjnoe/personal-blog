dd58f72826b97ef29b5f4c4337b40fcffb0f7563
Tommy Noe <tnoe@levdigital.com>
2020-01-05 21:19:12 -0500

    add about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+export default () => (
+  <Layout>
+    <h1>About</h1>
+    <div>
+      <p>
+        Hey! I'm Tommy Noe, a Salesforce Development Manager by day, general web developer by night from Indianapolis, IN. I post some things I'm learning and working on here. Hope you enjoy!
+      </p>
+    </div>
+  </Layout>
+)
