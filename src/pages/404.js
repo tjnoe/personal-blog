f592761444b6e0f9cdfa594e849cea2f389d8d77
tjnoe <tjnoe01@gmail.com>
2020-01-06 21:05:59 -0500

    add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/layout'
+import { Link } from 'gatsby'
+
+const BadRoute = () => (
+  <Layout>
+    <h1>Page Not Found</h1>
+    <p>
+      Oh no! You tried to access a page that didn't exist. Try heading back to the {" "} 
+      <Link to={`/`}>home page</Link>.
+    </p>
+  </Layout>
+)
+
+export default BadRoute
